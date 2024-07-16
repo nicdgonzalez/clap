332a80f0da985b767c00c579dd08f64e3eb2427e
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-07-16 18:15:40 -0400

    Update example so min and max are Positionals

--- examples/fizzbuzz.py
+++ examples/fizzbuzz.py
@@ -8,10 +8,10 @@ import clap
 @clap.script()
 def fizzbuzz(
     # positional arguments are converted into Positionals
+    min: int = 1,
+    max: int = 15,
     *,
     # keyword-only arguments are converted into Options
-    min: Annotated[int, clap.Alias("a")] = 1,
-    max: Annotated[int, clap.Alias("b")] = 15,
     skip_empty: Annotated[bool, clap.Alias("s")] = False,
 ) -> None:
     """A simple FizzBuzz implementation to demo `clap.Script`!
